66ce80a7461b5ddabdc0c0172b515e8944ef1c9d
the_ds <128127664+thedeepaksingh@users.noreply.github.com>
2025-09-06 16:02:45 +0530

    Services configured

--- libs/common/src/schemas/user.schema.ts
+++
@@ -1,7 +0,0 @@
-// libs/common/src/schemas/user.schema.ts
-import { Schema } from "mongoose";
-
-export const UserSchema = new Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
