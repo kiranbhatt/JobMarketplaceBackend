02e89fbc316791b1abad0374007dd3c1da00e065
Kiran Bhatt <57283107+kiranbhatt@users.noreply.github.com>
2025-09-14 18:17:29 +0530

    testcommit (#3)

---
+++ apps/auth/src/auth/interface/auth-tokens.interface.ts
@@ -0,0 +1,6 @@
+// src/auth/interface/auth-tokens.interface.ts
+
+export interface AuthTokens {
+  accessToken: string;
+  refreshToken: string;
+}
---
+++ apps/auth/src/dto/refresh-token.dto.ts
@@ -0,0 +1,7 @@
+// src/auth/dto/refresh-token.dto.ts
+import { IsNotEmpty } from 'class-validator';
+
+export class RefreshTokenDto {
+  @IsNotEmpty()
+  refreshToken: string;
+}
